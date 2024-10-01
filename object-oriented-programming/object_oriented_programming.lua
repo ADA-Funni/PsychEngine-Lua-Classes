abdad15cdabf56f24e31fe6fc39f9cb4966f9d39
ooftheperson <99497910+ADA-Funni@users.noreply.github.com>
2024-10-01 17:44:51 +0100

    Update object_oriented_programming.lua

--- object-oriented-programming/object_oriented_programming.lua
+++ object-oriented-programming/object_oriented_programming.lua
@@ -63,3 +63,43 @@ This function moves your object to the center of the screen.
 function YourObject:screenCenter(axes)
     screenCenter(self.tag, axes)
 end
+
+---@param name string
+---@param prefix string
+---@param framerate number
+---@param loop boolean
+--[[
+This function adds an animation to your object.
+]]--
+function YourObject:addAnimationByPrefix(name, prefix, framerate, loop)
+    addAnimationByPrefix(self.tag, name, prefix, framerate, loop)
+end
+
+---@param name string
+---@param prefix string
+---@param indices string
+---@param framerate number
+---@param loop boolean
+--[[
+This function adds an animation to your object.
+
+`indices` works like "0,1,2,3,4,5".
+]]--
+function YourObject:addAnimationByIndices(name, prefix, indices, framerate, loop)
+    if loop == true then
+        addAnimationByIndicesLoop(self.tag, name, prefix, indices, framerate)
+    else
+        addAnimationByIndices(self.tag, name, prefix, indices, framerate)
+    end
+end
+
+---@param name string
+---@param forced boolean
+---@param reverse boolean
+---@param startFrame number
+--[[
+This function plays an animation on your object.
+]]--
+function YourObject:playAnim(name, forced, reverse, startFrame)
+    playAnim(self.tag, name, forced, reverse, startFrame)
+end
